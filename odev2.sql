f5f50328ccc85d2801d07a4e8fb5e3e91db9c763
Yasin Koçer <92580565+yasinkocer@users.noreply.github.com>
2022-07-27 16:59:33 +0300

    Update odev2.sql

--- odev2.sql
+++ odev2.sql
@@ -1,3 +1,3 @@
 -- 2 -) country tablosunda bulunan country sütunundaki ülke isimlerinden en az
 -- 6 karakterden oluşan ve sonu 'n' karakteri ile sonlananları sıralayınız.
-SELECT country FROM country WHERE country LIKE '______%n'
+SELECT country FROM country WHERE length(country) >= 6 AND lower(country) like '%n';
